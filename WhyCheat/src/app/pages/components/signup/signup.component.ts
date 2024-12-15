d60bceb16e9c8268556a630e7ece4d1eef3fe3da
HamzaLatifGIT <hamzalatif006@gmail.com>
2024-12-16 01:02:03 +0500

    user Register Api Call

--- WhyCheat/src/app/pages/components/signup/signup.component.ts
+++ WhyCheat/src/app/pages/components/signup/signup.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../core/services/user.service';
+
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
 SideImg:string='signup-side-img.jpg'
+// userService = inject(UserService)
+userData:any={
+  "userName":"",
+  "email":"",
+  "password":""
+  
+  
+  
+}
+constructor(private userservice: UserService) {}
+
+onSubmit(){
+  this.userservice.onSignup(this.userData).subscribe((res:any)=>{
+    console.log(res)
+  })
+}
 }
